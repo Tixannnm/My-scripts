b230bd0e0445fdb5608fe0ae6608dc56e4e0a04d
Tixannnm <133684746+Tixannnm@users.noreply.github.com>
2025-10-24 14:39:31 +0300

    Update Evade AutoFarm.lua

--- Evade AutoFarm.lua
+++ Evade AutoFarm.lua
@@ -1,14 +1,16 @@
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame =
     CFrame.new(0, 10000, 0)
 workspace.Gravity = 0
-while task.wait(1) do
+while task.wait(0.5) do
     local player = game:GetService('Players').LocalPlayer.Character
     for i, v in workspace.Game.Effects.Tickets:GetChildren() do
         if v.Name == 'Visual' then
             player:PivotTo(v:GetPivot())
+            task.wait(0.3)
+            player:PivotTo(v:GetPivot() + Vector3.new(0, 13, 0))
             task.wait(1)
             game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame =
-                CFrame.new(0, 10000, 0)
+    CFrame.new(0, 10000, 0)
         end
     end
 end
