b27d99d3f446eb9d1454fe364cfe74a9e0234c45
Tixannnm <133684746+Tixannnm@users.noreply.github.com>
2025-10-24 11:01:28 +0300

    Update Evade AutoFarm.lua

--- Evade AutoFarm.lua
+++ Evade AutoFarm.lua
@@ -1,7 +1,7 @@
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame =
     CFrame.new(0, 10000, 0)
 workspace.Gravity = 0
-while task.wait(0.5) do
+while task.wait(1) do
     local player = game:GetService('Players').LocalPlayer.Character
     for i, v in workspace.Game.Effects.Tickets:GetChildren() do
         if v.Name == 'Visual' then
