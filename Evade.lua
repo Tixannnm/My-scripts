6b397a8a9cc8340732c58eb35b17552a6035b83b
Tixannnm <133684746+Tixannnm@users.noreply.github.com>
2025-10-24 12:29:54 +0300

    Create Evade.lua

---
+++ Evade.lua
@@ -0,0 +1,18 @@
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame =
+    CFrame.new(0, 10000, 0)
+workspace.Gravity = 0
+while task.wait(1) do
+    local player = game:GetService('Players').LocalPlayer.Character
+    for i, v in workspace.Game.Effects.Tickets:GetChildren() do
+        if v.Name == 'Visual' then
+            player:PivotTo(v:GetPivot())
+            task.wait(1)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame =
+                CFrame.new(0, 10000, 0)
+        end
+    end
+    local n1 = [[
+    !specialround PlushieHell
+    ]]
+    game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("\r" .. n1:gsub("\n", "\r") .. "")
+end
