5b9922bb3bace23147e1cfb25d5b38019e0caaf7
Tixannnm <133684746+Tixannnm@users.noreply.github.com>
2025-10-24 18:15:41 +0300

    Update Evade AutoFarm.lua

--- Evade AutoFarm.lua
+++ Evade AutoFarm.lua
@@ -1,7 +1,8 @@
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame =
+    CFrame.new(0, 500, 0)
 workspace.Gravity = 0
-local player = game:GetService('Players').LocalPlayer.Character
-player.HumanoidRootPart.CFrame = CFrame.new(0, 500, 0)
 while task.wait(1) do
+    local player = game:GetService('Players').LocalPlayer.Character
     for i, v in workspace.Game.Effects.Tickets:GetChildren() do
         if v.Name == 'Visual' then
             player:PivotTo(v:GetPivot())
@@ -10,21 +11,8 @@ while task.wait(1) do
             workspace.Gravity = 0
             player:PivotTo(v:GetPivot() + Vector3.new(0, 13, 0))
             task.wait(1)
-            player.HumanoidRootPart.CFrame = CFrame.new(0, 500, 0)
-            task.wait(1)
-            player.HumanoidRootPart.CFrame = CFrame.new(500, 500, 0)
-            task.wait(1)
-            player.HumanoidRootPart.CFrame = CFrame.new(-500, 500, 0)
-            task.wait(1)
-            player.HumanoidRootPart.CFrame = CFrame.new(0, 500, 0)
-            task.wait(1)
-            player.HumanoidRootPart.CFrame = CFrame.new(500, 500, 0)
-            task.wait(1)
-            player.HumanoidRootPart.CFrame = CFrame.new(-500, 500, 0)
-            task.wait(1)
-            player.HumanoidRootPart.CFrame = CFrame.new(500, 500, 0)
-            task.wait(1)
-            player.HumanoidRootPart.CFrame = CFrame.new(0, 500, 0)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame =
+                CFrame.new(0, 500, 0)
         end
     end
 end
