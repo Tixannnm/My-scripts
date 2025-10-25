e11984f2b41a324ba317834d083774ac1576cccd
Tixannnm <133684746+Tixannnm@users.noreply.github.com>
2025-10-25 20:50:38 +0300

    Update Evade AutoFarm.lua

--- Evade AutoFarm.lua
+++ Evade AutoFarm.lua
@@ -5,15 +5,12 @@ while task.wait(1) do
     local pos1 = CFrame.new(0, 500, 0)
     local pos2 = CFrame.new(500, 150, 0)
     local pos3 = CFrame.new(-500, 400, 0)
+    hrp.CFrame = pos1
+    hrp.CFrame = pos1
     if player then
-        hrp.CFrame = pos1
         for i, v in workspace.Game.Effects.Tickets:GetChildren() do
             if v.Name == 'Visual' then
-                player:PivotTo(v:GetPivot())
-                workspace.Gravity = 196
-                task.wait(0.1)
-                workspace.Gravity = 0
-                player:PivotTo(v:GetPivot() + Vector3.new(0, 13, 0))
+                player:PivotTo(v:GetPivot() + Vector3.new(0, 10, 0))
                 task.wait(1)
                 hrp.CFrame = pos1 task.wait(.5) hrp.CFrame = pos2 task.wait(.5) hrp.CFrame = pos3 task.wait(.5) hrp.CFrame = pos2 task.wait(.5) hrp.CFrame = pos3 task.wait(.5) hrp.CFrame = pos1 task.wait(.5) hrp.CFrame = pos2 task.wait(.5) hrp.CFrame = pos3 task.wait(.5)
                 hrp.CFrame = pos2 task.wait(.5) hrp.CFrame = pos1 task.wait(.5) hrp.CFrame = pos2 task.wait(.5) hrp.CFrame = pos3 task.wait(.5) hrp.CFrame = pos2 task.wait(.5) hrp.CFrame = pos3 task.wait(.5) hrp.CFrame = pos1 task.wait(.5) hrp.CFrame = pos2 task.wait(.5) hrp.CFrame = pos3 task.wait(.5) hrp.CFrame = pos2 
