7ca4217e8c4d143703093fa5e678e6f29a64c13d
Tixannnm <133684746+Tixannnm@users.noreply.github.com>
2025-11-09 13:52:18 +0200

    Update Evade Money AutoFarm.lua

--- Evade Money AutoFarm.lua
+++ Evade Money AutoFarm.lua
@@ -1,13 +1,15 @@
-local player = game:GetService('Players').LocalPlayer.Character
-local hrp = player:WaitForChild('HumanoidRootPart')
-local pos1 = CFrame.new(-225, -22, -131)
-hrp.CFrame = pos1
 workspace.Gravity = 0
-while task.wait(5) do
+while task.wait(1) do
+    local player = game:GetService('Players').LocalPlayer.Character
+    local hrp = player:WaitForChild('HumanoidRootPart')
+    local pos1 = CFrame.new(-225, -22, -131)
     hrp.CFrame = pos1
     if player then
-        hrp.CFrame = pos1
-        local n1 = [[!specialround Plushie Hell]]
-        game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("\r" .. n1:gsub("\n", "\r") .. "")
+        for a = 1,14 do
+            hrp.CFrame = pos1
+            wait(0.5)
+        end
+        local n1 = "!specialround Plushie Hell"
+        game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync(n1)
     end
 end
